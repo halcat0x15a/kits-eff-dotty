8c671e67a2ed60d31a38bb63ae7b7671514a19c4
sanshiro_yoshida <sanshiro_yoshida@dwango.co.jp>
2018-01-04 22:18:26 +0900

    tailrec

--- src/main/scala/kits/eff/Arrs.scala
+++ src/main/scala/kits/eff/Arrs.scala
@@ -2,16 +2,20 @@ package kits.eff
 
 import scala.annotation.tailrec
 
-enum class Arrs[+R, A, B] {
-  def apply(a: A): Eff[R, B] = view match {
-    case Arrs.View.One(f) => f(a)
-    case Arrs.View.Cons(f, g) => f(a) match {
-      case Eff.Pure(b) => g(b)
-      case Eff.Impure(r, h) => Eff.Impure(r, h ++ g)
-    }
+enum class Arrs[+R, A, B] extends (A => Eff[R, B]) {
+  def apply(a: A): Eff[R, B] = {
+    @tailrec def go(arrs: Arrs[R, Any, B], a: Any): Eff[R, B] =
+      arrs.view match {
+        case Arrs.View.One(f) => f(a)
+        case Arrs.View.Cons(f, g) => f(a) match {
+          case Eff.Pure(b) => go(g, b)
+          case Eff.Impure(r, h) => Eff.Impure(r, h ++ g)
+        }
+      }
+    go(this.asInstanceOf[Arrs[R, Any, B]], a)
   }
 
-  def view[RR >: R]: Arrs.View[RR, A, B]
+  def view: Arrs.View[R, A, B]
 
   def :+[S, C](f: B => Eff[S, C]): Arrs[R | S, A, C] = Arrs.Node(this, Arrs.Leaf(f))
 
@@ -19,23 +23,23 @@ enum class Arrs[+R, A, B] {
 }
 
 object Arrs {
+  enum View[+R, A, B] {
+    case One[R, A, B](arr: A => Eff[R, B]) extends View[R, A, B]
+    case Cons[R, A, B, C](arr: A => Eff[R, B], arrs: Arrs[R, B, C]) extends View[R, A, C]
+  }
+
   case Leaf[R, A, B](arr: A => Eff[R, B]) extends Arrs[R, A, B] {
-    def view[RR >: R] = View.One(arr)
+    def view = View.One(arr)
   }
 
   case Node[R, A, B, C](left: Arrs[R, A, B], right: Arrs[R, B, C]) extends Arrs[R, A, C] {
-    def view[RR >: R] = {
-      @tailrec def go(left: Arrs[RR, A, Any], right: Arrs[RR, Any, C]): View[RR, A, C] =
+    def view = {
+      @tailrec def go(left: Arrs[R, A, Any], right: Arrs[R, Any, C]): View[R, A, C] =
         left match {
           case Leaf(arr) => View.Cons(arr, right)
           case Node(l, r) => go(l, r ++ right)
         }
-      go(left.asInstanceOf[Arrs[RR, A, Any]], right.asInstanceOf[Arrs[RR, Any, C]])
+      go(left.asInstanceOf[Arrs[R, A, Any]], right.asInstanceOf[Arrs[R, Any, C]])
     }
-  }
-
-  enum View[R, A, B] {
-    case One[R, A, B](arr: A => Eff[R, B]) extends View[R, A, B]
-    case Cons[R, A, B, C](arr: A => Eff[R, B], arrs: Arrs[R, B, C]) extends View[R, A, C]
   }
 }
